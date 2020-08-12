47fc958742ebbf5f80768602461aae9b61d8948f
中村昌暉 <smith@nakamuramasaakiranoMacBook-Pro.local>
2020-08-13 02:43:00 +0900

    Updated dashboard page

--- pages/user/dashboard.vue
+++ pages/user/dashboard.vue
@@ -1,9 +1,27 @@
 <template>
-    <div>Dashboard</div>
+    <div class='userAction'>
+        <div>
+            Dashboard
+        </div>
+        <div class='userAction_logout userAction_common'>
+            <a href="#" @click.prevent="logout">
+                <i class="fas fa-cog fa-2x"></i>
+                <span>ログアウト</span>
+            </a>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-    middleware: ['auth']
+    middleware: ['auth'],
+    methods: {
+        logout(){
+            this.$auth.logout();
+        }
+    }
 };
 </script>
+
+<style lang="scss" scoped>
+</style>
